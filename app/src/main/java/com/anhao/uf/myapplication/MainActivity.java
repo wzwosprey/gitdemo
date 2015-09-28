d470c90185d34d91b15ec2b59fa4ecc89b2b2d8d
wangzw1020 <836156120@qq.com>
2015-09-28 15:53:25 +0800

    第三次修改提交测试

--- app/src/main/java/com/anhao/uf/myapplication/MainActivity.java
+++ app/src/main/java/com/anhao/uf/myapplication/MainActivity.java
@@ -1,9 +1,23 @@
 package com.anhao.uf.myapplication;
 
+import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
+import android.widget.TextView;
 
 
 public class MainActivity extends ActionBarActivity {
 
     //测试修改  提交到github
+    private TextView tv_test;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        initView();
+    }
+
+    private void initView() {
+        tv_test = (TextView) findViewById(R.id.tv);
+        tv_test.setText("qzqzqqzqjqzqqzq");
+    }
 }
